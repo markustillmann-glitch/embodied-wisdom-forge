eab9c0b5284fd75ab3d0bb0de4d4bb4754dd9822
Martin Dilg <martin.dilg@gmail.com>
2026-03-04 23:10:17 +0100

    Initial iOS project setup with Capacitor and Swift Package Manager integration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.65170ef8771a47c2986b98a79873d15f',
+  appId: 'de.lunabit.selfcareReflection',
   appName: 'Selfcare Reflection',
-  webDir: 'dist',
-  server: {
-    url: 'https://65170ef8-771a-47c2-986b-98a79873d15f.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
